a9cb289cff208949e65a27e9d0f0ce7a8a56c72d
hein-riley <Riley.Hein@rizepoint.com>
2018-01-18 12:14:57 -0700

    new personal project

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Julius+Sans+One');
+	
+	.mt-wrapper h1{
+		font-family: Julius Sans One;
+		font-size: 5rem;
+		text-align: center;
+		margin: 10px 0px;
+	}
+	
+	.mn-wrapper{
+		text-align: center;
+	}
+	
+	.mn-wrapper ul, li{
+		list-style: none;
+		display: inline-flex;
+		padding: 0px;
+	}
+	
+	.main-body-wrapper img {
+		width: 100%;
+		max-width: 1200px;
+		margin: 0px auto;
+	}
+	
+	.triangle-up {
+		width: 0;
+		height: 0;
+		border-left: 50px solid transparent;
+		border-right: 50px solid transparent;
+		border-bottom: 100px solid red;
+}
+	
+	.triangle-down {
+		width: 0;
+		height: 0;
+		border-left: 50px solid transparent;
+		border-right: 50px solid transparent;
+		border-top: 100px solid red;
+}
+	
+	
+</style>	
+	
+	
+
+<body>
+	<div class="container-fluid main-title">
+		<div class="container mt-wrapper">
+			<h1>Text</h1>
+		</div><!--mt-wrapper-->
+	</div><!--main-title-->
+	<div class="container-fluid main-nav">
+		<div  class="container mn-wrapper">
+			<ul>
+				<li>1</li>
+				<li>2</li>
+				<li>3</li>
+				<li>4</li>
+			</ul>
+		</div><!--mn-wrapper-->
+	</div><!--main-nav-->
+	<div class="container-fluid main-body">
+		<div class="container main-body-wrapper">
+			<img src="https://hdwallsource.com/img/2016/7/factory-desktop-wallpaper-53902-55633-hd-wallpapers.jpg">
+			<div class="triangle-up">
+			
+			</div>
+			<div class"triangle-up">
+			
+			</div>
+			<div class="triangle-up">
+			
+			</div>
+		</div><!--main-body-->
+	</div><!--main-body-->
+	<div class="container-fluid main-footer">
+		<div class="container mf-wrapper">
+			<p>Footstuff</p>
+		</div><!--mf-wrapper-->
+	</div><!--main-footer-->
+</body>
+</html>

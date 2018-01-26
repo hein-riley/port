e94642b6668d63bcf4c780d3899d7045c0457869
hein-riley <rjhein3@gmail.com>
2018-01-25 23:42:27 -0700

    site update 1-25-18

--- index.php
+++ index.php
@@ -1,171 +1,16 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<title></title>
-</head>
+<?php include("includes/header.php");?>
 
-<style>
-
-@import url('https://fonts.googleapis.com/css?family=Montserrat:100, 300');
-    
-   body{
-        
-        background-color: #f5f4ff;
-        background: -moz-linear-gradient(top, #f5f4ff 0%, #e9e8ff 0%, #8ec7ce 100%);
-        background: -webkit-linear-gradient(top, #f5f4ff 0%,#e9e8ff 0%,#8ec7ce 100%);
-        background: linear-gradient(to bottom, #f5f4ff 0%,#e9e8ff 0%,#8ec7ce 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f5f4ff', endColorstr='#8ec7ce',GradientType=0 );
-    }
-    
-    .mt-wrapper{
-        padding: 40px;
-    }
-    
-    .mt-wrapper a{
-        
-        text-decoration: none;
-        color: gray; 
-    }
-    
-    .mt-wrapper a:hover{
-        text-decoration: none;
-        color: lightcoral; 
-    }
-    
-    .mn-wrapper ul a{
-        text-decoration: none;
-        color: gray;
-    }
-    
-    .mn-wrapper ul a:hover{
-        text-decoration: none;
-        color: lightcoral;
-    }
-    
-    .main-nav{
-        
-    }
-    
-    .mn-wrapper{
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
-    
-    .mn-wrapper{
-		text-align: center;
-	}
-	
-	.mn-wrapper ul, li{
-		list-style: none;
-		display: inline-flex;
-        padding: 0px 70px 35px;
-	}
-	
-	.mt-wrapper h1{
-		font-family: Montserrat;
-		font-size: 5rem;
-		text-align: center;
-		margin: 10px 0px;
-	}
-    
-    .main-body-wrapper{
-        padding: 10px;
-        margin: 0 auto;
-    }
-    
-    .mb-content{
-        padding: 10px;
-        background: #eee;
-        max-width: 100%;
-        width: 500px;
-        margin: 35px;
-        box-shadow: 2px 2px 2px grey;
-    }
-	
-	.main-body-wrapper img {
-		width: 100%;
-		max-width: 1200px;
-		margin: 0px auto;
-	}
-	
-    .mbw-title{
-        text-align: center;
-        font-weight: 200;
-    }
-    
-    .mbw-title a{
-        text-decoration: none;
-        color: gray;
-    }
-    
-    .mbw-title a h4{
-        font-weight: 300;
-        font-size: 2rem;
-    }
-    
-    .mbw-title a:hover{
-        text-decoration: none;
-        color: lightblue;
-    }
-    
-    .resume-wrapper, .about-wrapper{
-        text-align: center;
-        font-family: Montserrat;
-        font-size: 2rem;
-        padding: 150px 0px 250px;
-    }
-    
-    .about-wrapper a{
-        text-decoration: none;
-    }
-    
-    .resume-wrapper a{
-        text-decoration: none;
-    }
-    
-    .resume-wrapper a p:hover{
-        color:lightcoral;
-    }
-    
-    .about-wrapper a p:hover{
-        color:lightcoral;
-    }
-	
-    .container.mf-wrapper p {
-        text-align: center;
-        color: gray;
-}
-    
-</style>	
-	
-	
-
-<body>
-	<div class="container-fluid main-title">
-		<div class="container mt-wrapper">
-            <a href="index.php"><h1>RILEY HEIN</h1></a>
-		</div><!--mt-wrapper-->
-	</div><!--main-title-->
-	<div class="container-fluid main-nav">
-		<div  class="container mn-wrapper">
-			<ul>
-				<a href="index.php"><li>Home</li></a>
-				<a href="#"><li>Portfolio</li></a>
-				<a href="#"><li>About</li></a>
-				<a href="#"><li>Contact</li></a>
-			</ul>
-		</div><!--mn-wrapper-->
-	</div><!--main-nav-->
-	<div class="container-fluid main-body">
+<div class="container-fluid main-body">
         <div class="container main-body-wrapper">
             
+        <div class="mt-wrapper">  
+            <h1>Portfolio</h1>
+        </div><!--mt-wrapper-->   
+            
             <div class="col-md-6 col-sm-12 col-xs-12 mb-content">
-                <a href="#"><img src="http://d1marr3m5x4iac.cloudfront.net/images/edpborder500/I0-001/001/019/757-1.jpeg_/ice-cube-57.jpeg"></a>
+                <a href="portfolio.php"><img src="img/newsite2cropped.png"></a>
             <div class="mbw-title">
-                <a href="#"><h4>Rizepoint.com</h4></a>
+                <a href="portfolio.php"><h4>Rizepoint.com</h4></a>
             </div><!--mbw-title-->
             </div><!--mb-content-->
         
@@ -188,17 +33,9 @@
             </div>
     
             <div class="col-md-6 col-sm-12 col-xs-12 about-wrapper">
-                <a href="#"><p>About Riley</p></a>
+                <a href="about.php"><p>About Riley</p></a>
             </div>
         </div><!--info-body-wrapper-->
     </div><!--info-body-->
-    
-    
-	<div class="container-fluid main-footer">
-		<div class="container mf-wrapper">
-			<p>All rights reserved.  Content &copy; <?php echo date('Y'); ?> Riley Hein </p>
-		</div><!--mf-wrapper-->
-	</div><!--main-footer-->
-    
-</body>
-</html>
+
+<?php include("includes/footer.php");?>
--- portfolio.php
+++ portfolio.php
@@ -1,208 +1,30 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<title></title>
-</head>
+<?php include("includes/header.php");?>
 
-<style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat:100, 300');
-    
-   body{
-        
-        background-color: #f5f4ff;
-        background: -moz-linear-gradient(top, #f5f4ff 0%, #e9e8ff 0%, #8ec7ce 100%);
-        background: -webkit-linear-gradient(top, #f5f4ff 0%,#e9e8ff 0%,#8ec7ce 100%);
-        background: linear-gradient(to bottom, #f5f4ff 0%,#e9e8ff 0%,#8ec7ce 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f5f4ff', endColorstr='#8ec7ce',GradientType=0 );
-    }
-    
-    .mt-wrapper{
-        padding: 40px;
-    }
-    
-    .mt-wrapper a{
-        
-        text-decoration: none;
-        color: gray; 
-    }
-    
-    .mt-wrapper a:hover{
-        text-decoration: none;
-        color: lightcoral; 
-    }
-    
-    .mn-wrapper ul a{
-        text-decoration: none;
-        color: gray;
-    }
-    
-    .mn-wrapper ul a:hover{
-        text-decoration: none;
-        color: lightcoral;
-    }
-    
-    .main-nav{
-        
-    }
-    
-    .mn-wrapper{
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
-    
-    .mn-wrapper{
-		text-align: center;
-	}
-	
-	.mn-wrapper ul, li{
-		list-style: none;
-		display: inline-flex;
-        padding: 0px 70px 35px;
-	}
-	
-	.mt-wrapper h1{
-		font-family: Montserrat;
-		font-size: 5rem;
-		text-align: center;
-		margin: 10px 0px;
-	}
-    
-    .main-body-wrapper{
-        padding: 10px;
-        margin: 0 auto;
-    }
-    
-    .mb-content{
-        padding: 10px;
-        background: #eee;
-        max-width: 100%;
-        width: 500px;
-        margin: 35px;
-        box-shadow: 2px 2px 2px grey;
-    }
-	
-	.main-body-wrapper img {
-		width: 100%;
-		max-width: 1200px;
-		margin: 0px auto;
-	}
-	
-    .mbw-title{
-        text-align: center;
-        font-weight: 200;
-    }
-    
-    .mbw-title a{
-        text-decoration: none;
-        color: gray;
-    }
-    
-    .mbw-title a h4{
-        font-weight: 300;
-        font-size: 2rem;
-    }
-    
-    .mbw-title a:hover{
-        text-decoration: none;
-        color: lightblue;
-    }
-    
-    .resume-wrapper, .about-wrapper{
-        text-align: center;
-        font-family: Montserrat;
-        font-size: 2rem;
-        padding: 150px 0px 250px;
-    }
-    
-    .about-wrapper a{
-        text-decoration: none;
-    }
-    
-    .resume-wrapper a{
-        text-decoration: none;
-    }
-    
-    .resume-wrapper a p:hover{
-        color:lightcoral;
-    }
-    
-    .about-wrapper a p:hover{
-        color:lightcoral;
-    }
-	
-    .container.mf-wrapper p {
-        text-align: center;
-        color: gray;
-}
-    
-/*
-  portfolio  
-*/
-
-    .port-title h1 {
-        text-align: center;
-        padding-bottom: 80px;
-        font-family: montserrat;
-}
-    
-    .cont-col1{
-        padding-bottom: 100px;
-    }
-    
-    .cont-col1 p{
-        font-size: 2rem;
-    }
-    
-    .cont-col2 img {
-    vertical-align: center;
-    width: 100%;
-    padding-bottom: 100px;
-}
-    
-/*
-   Portfolio  
-*/
-    
-    .stats p {
-    font-size: 2rem;
-    background: lightcoral;
-    padding: 50px;
-    text-align: center;
-    box-shadow: 3px 3px 3px grey;
-    color: white;
-}
-    
-</style>	
-	
-	
-
-<body>
-	<div class="container-fluid main-title">
-		<div class="container mt-wrapper">
-            <a href="index.php"><h1>RILEY HEIN</h1></a>
-		</div><!--mt-wrapper-->
-	</div><!--main-title-->
-	<div class="container-fluid main-nav">
-		<div  class="container mn-wrapper">
-			<ul>
-				<a href="index.php"><li>Home</li></a>
-				<a href="#"><li>Portfolio</li></a>
-				<a href="#"><li>About</li></a>
-				<a href="#"><li>Contact</li></a>
-			</ul>
-		</div><!--mn-wrapper-->
-	</div><!--main-nav-->
 	<div class="container-fluid main-body">
         <div class="col-md-12 col-sm-12 col-xs-12 port-title">
                 <h1>Rizepoint Case Study</h1>
             </div><!--port-title-->
         
         <div class="container main-body-wrapper">
-            
-             <div class="mb-wrapper">
+                <h1>Summary</h1>
+
+            <section class="section1">
+                <div class="col-md-12 col-sm-12 col-xs-12 cont-col1">
+                       <h2>Scenario:</h2>
+                    <p>
+                        In January 2017 I was brought onto the Rizepoint marketing team to help with a total site redesign and build.
+                        <br><br>
+                        Rizepoint is a corporate compliance software company. Their products are used across many industries to manage brand, keep products within regulated parameters and maintain internal QA standards.
+                        <br><br>
+                        Rizepoint was seeking a site redesign, as their active website was not acting to the capacity desired. Users weren't utilizing the tools the site provided. Potential customers weren't finding information about the product and they were leaving the site after only a short time.
+                        <br><br>
+                        Key to the redesign was that the website would operate as a salesman to potential clients. Every part of the site was considered, studied and tested to maximize user engagement, retention and action.
+ 
+                    </p>
+                </div><!--cont-col1-->
+                
+                <h2>Outcome:</h2>
+            <div class="mb-wrapper">
                 <div class="col-md-3 col-sm-6 col-xs-12 stats">
                     <p>User bounce rate decreased by 22% over 2016</p>
                 </div>
@@ -213,25 +35,13 @@
                     <p>Page views increased 24% year over year</p>
                 </div>
                 <div class="col-md-3 col-sm-6 col-xs-12 stats">
-                    <p>19% of sales driven by website leads in 2017</p>
+                    <p>19% of top sales driven by website leads in 2017</p>
                 </div>
             </div><!--mb-wrapper-->
 
-
-            <section class="section1">
-                <div class="col-md-12 col-sm-12 col-xs-12 cont-col1">
-                       <h2>Setup:</h2>
-                    <p>
-                        Rizepoint is a corporate compliance software company. Their products are used across many industries to manage brand, keep products within regulated parameters and maintain internal QA standards.
-                        <br><br>
-                        In February I was brought onto the marketing team to help with a total site redesign and build.
-                        <br><br>
-                        Key to the redesign was that the website would operate as a salesman to potential clients. Every part of the site was considered, studied and tested to maximize user engagement, retention and action.
- 
-                    </p>
-                </div><!--cont-col1-->
-
                 <div class="col-md-6 col-sm-12 col-xs-12 cont-col1">
+                    
+                        <h1>In Depth Review:</h1>
 
                        <h2>Navigation:</h2>
                     <p>
@@ -243,12 +53,7 @@
 
                     </p>
                 </div><!--cont-col1-->
-            
-            <div class="cont">
-                <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
-                    <img src="img/oldmenu4.png">
-                </div><!--cont-col2-->
-            </div><!--content-wrapper-->
+
             <div class="cont">
                 <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
                     <img src="img/oldmenu3.png">
@@ -259,6 +64,13 @@
             
             
         <section class="section2">  
+            <div class="cont">
+                <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
+                    <img src="img/newnav1.png">
+                </div><!--cont-col2-->
+            </div><!--content-wrapper-->
+            
+            
             <div class="col-md-6 col-sm-12 col-xs-12 cont-col1">
 
                     <p>
@@ -275,10 +87,10 @@
                 </div><!--cont-col1-->
             
             <div class="cont">
-                    <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
-                        <img src="img/newnav1.png">
-                    </div><!--cont-col2-->
-                </div><!--content-wrapper-->
+                <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
+                    <img src="img/newnav1.png">
+                </div><!--cont-col2-->
+            </div><!--content-wrapper-->
             
                 <div class="cont">
                     <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
@@ -302,16 +114,15 @@
 
                     </p>
                 </div><!--cont-col1-->
-            
             <div class="cont">
-                    <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
+                    <div class="col-md-12 col-sm-12 col-xs-12 cont-col2">
                         <img src="img/newsite1.png">
                     </div><!--cont-col2-->
                 </div><!--content-wrapper-->
             
                 <div class="cont">
                     <div class="col-md-12 col-sm-12 col-xs-12 cont-col2">
-                        <img src="img/newsite12.png">
+                        <img src="img/newsite2.png">
                     </div><!--cont-col2-->
                 </div><!--content-wrapper-->
                 <div class="cont">
@@ -330,23 +141,25 @@
                     </div><!--cont-col2-->
                 </div><!--content-wrapper-->
             
+                <div class="cont">
+                    <div class="col-md-6 col-sm-12 col-xs-12 mb-content2">
+                        <img src="img/marcom.png">
+                    </div><!--cont-col2-->
+                </div><!--cont-->  
+                <div class="cont">
+                    <div class="col-md-6 col-sm-12 col-xs-12 cont-col2">
+                        <h3>Winner of Marcom Gold Award for website redesign in 2017</h3>
+                    </div><!--cont-col2-->
+                </div><!--cont--> 
+            
         </section>
+        
             
             
             
             
 	   </div><!--main-body-wrapper-->
     </div><!--main-body-->
+
     
-    
-    
-    
-    
-	<div class="container-fluid main-footer">
-		<div class="container mf-wrapper">
-			<p>All rights reserved.  Content &copy; <?php echo date('Y'); ?> Riley Hein </p>
-		</div><!--mf-wrapper-->
-	</div><!--main-footer-->
-    
-</body>
-</html>
+<?php include("includes/footer.php");?>
